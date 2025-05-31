37fe350a219d02dc58cce6d9afdefc17336772c7
AAdedotun <judeawolaja@gmail.com>
2025-05-31 09:11:09 +0100

    Preview card

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-yellow': 'hsl(47, 88%, 63%)',
+      }
+    },
+  },
+  plugins: [],
+}
+
